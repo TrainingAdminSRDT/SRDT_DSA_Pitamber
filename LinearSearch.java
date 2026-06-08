d2f3194f3dee4f00bb2c23f5ac38b8e59e56d607
Tejas Pratap Singh <tejasprataps891@gmail.com>
2026-06-09 00:21:35 +0530

    Added Linear Search solution

---
+++ LinearSearch.java
@@ -0,0 +1,12 @@
+public class LinearSearch {
+     public static void main(String[] args) {
+         int arr[] = {5, 10, 15, 20, 25};
+          int key = 20;
+           for (int i = 0; i < arr.length; i++) {
+             if (arr[i] == key) {
+                System.out.println("Element found at index " + i);
+                return;
+            }
+        }System.out.println("Element not found");
+    }
+}
