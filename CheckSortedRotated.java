3408678f40d7a4f8a06b2f9226b238b39736e415
Vaishali Srivastava <srivastavavaishali81@gmail.com>
2026-06-10 00:08:03 +0530

    Added First Last Occurence

---
+++ CheckSortedRotated.java
@@ -0,0 +1,23 @@
+public class CheckSortedRotated {
+
+    public static boolean check(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+
+        return count <= 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {3, 4, 5, 1, 2};
+        int[] nums2 = {2, 1, 3, 4};
+
+        System.out.println(check(nums1)); // true
+        System.out.println(check(nums2)); // false
+    }
+}
---
+++ FirstLastOccurence.java
@@ -0,0 +1,54 @@
+public class FirstLastOccurence {
+
+    public static int findFirst(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;
+                right = mid - 1; // continue searching left side
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static int findLast(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;
+                left = mid + 1; // continue searching right side
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 2, 3, 4, 5};
+        int target = 2;
+
+        int first = findFirst(arr, target);
+        int last = findLast(arr, target);
+
+        System.out.println("First occurrence: " + first);
+        System.out.println("Last occurrence: " + last);
+    }
+}
+
