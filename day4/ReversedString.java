92d36597170db0f3e3860a78f01a861bb2fa56eb
ShashankDubey Git <dev.shashankdubey@gmail.com>
2026-06-09 15:23:58 +0530

    Reversed String

---
+++ day4/ReversedString.java
@@ -0,0 +1,32 @@
+package day4;
+
+import java.util.Scanner;
+
+public class ReversedString { 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a string to reverse: ");
+        String input = scanner.nextLine(); 
+        
+        StringBuilder reversed = new StringBuilder();
+         
+        backtrackReverse(input, 0, reversed);
+        
+        System.out.println("Reversed string: " + reversed.toString());
+        
+        scanner.close(); 
+    }
+
+    private static void backtrackReverse(String str, int index, StringBuilder result) {
+        
+        if (index == str.length()) {
+            return;
+        }
+
+        
+        backtrackReverse(str, index + 1, result);
+        
+        result.append(str.charAt(index));
+    }
+}
