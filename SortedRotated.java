5f5519ef1fc8b1ae003b8d373613619097509620
Svara <svarasrivastava500@gmail.com>
2026-06-10 21:25:51 +0530

    SortedRotated

---
+++ SortedRotated.java
@@ -0,0 +1,21 @@
+public class SortedRotated {
+
+    public static boolean check(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+
+        return count <= 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 5, 1, 2};
+
+        System.out.println(check(arr));
+    }
+}
