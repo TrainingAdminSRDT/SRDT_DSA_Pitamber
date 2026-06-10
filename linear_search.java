74f1e89d7d0b11787487190d94870f2242447ac2
Svara <svarasrivastava500@gmail.com>
2026-06-10 13:17:54 +0530

    Added linear search implementation

---
+++ linear_search.java
@@ -0,0 +1,23 @@
+public class linear_search {
+    public static int findIndex(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; 
+            }
+        }
+        return -1; 
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int target = 30;
+
+        int index = findIndex(arr, target);
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
+
