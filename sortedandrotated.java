0eee416f6caca3a87ddb99f863d22619c27ecb09
ShreyagrawalCode <shreyagrawal7345@gmail.com>
2026-06-10 20:39:37 +0530

    sorted and rotated array

---
+++ sortedandrotated.java
@@ -0,0 +1,21 @@
+public class sortedandrotated {
+    public static boolean check(int[] nums) {
+        int count=0;
+        int n= nums.length;
+        for (int i = 0;i<n;i++) {
+            if (nums[i] >nums[(i +1)%n]) {
+                count++;
+            }
+            if (count >1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,1,2};
+        System.out.println(check(arr));
+    }
+}
+
