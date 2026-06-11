e626725c4fe66fc01f77a5ad5bfb226a6f259c23
Kritika Chaurasia <kchaurasia2204@gmail.com>
2026-06-11 23:58:30 +0530

    uploaded

---
+++ SearchinRotatedsortedArray.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SearchinRotatedsortedArray {
+    
+    public static boolean isSortedRotated(int[] arr) {
+        int count = 0;
+        int n = arr.length;
+
+        
+        if (n <= 1) return true;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > arr[(i + 1) % n]) {
+                count++;
+            }
+        }
+
+        return count == 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+       
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+       
+        Arrays.sort(arr);
+
+      
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        if (isSortedRotated(arr)) {
+            System.out.println("Array is Sorted and Rotated");
+        } else {
+            System.out.println("Array is Not Sorted and Rotated");
+        }
+
+       
+        scanner.close();
+    }
+}
