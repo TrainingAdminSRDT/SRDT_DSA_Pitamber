3b2e11b60c1bd641565cda04e8ad486b166e72fa
Divyanshu Mishra <yashmishra6444@gmail.com>
2026-06-11 13:47:15 +0530

    CircularQueue

--- Solution.java
+++ Solution.java
@@ -1,5 +1,51 @@
-public class Solution {
+class Solution {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] arr={23,12,45,34,67};
+        int l=0;
+        int r=arr.length-1;
+        mergeSort(arr, l, r);
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+
+        
+    }
+
+    public static void mergeSort(int[] arr, int l, int r) {
+
+        if (l >= r) return;
+
+        int mid = l + (r - l) / 2;
+
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+
+        merge(arr, l, mid, r);
+    }
+
+    static void merge(int[] arr, int l, int mid, int r) {
+
+        int[] temp = new int[r - l + 1];
+
+        int i = l;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= r) {
+            if (arr[i] <= arr[j])
+                temp[k++] = arr[i++];
+            else
+                temp[k++] = arr[j++];
+        }
+
+        while (i <= mid)
+            temp[k++] = arr[i++];
+
+        while (j <= r)
+            temp[k++] = arr[j++];
+
+        for (i = l, k = 0; i <= r; i++, k++) {
+            arr[i] = temp[k];
+        }
     }
 }
