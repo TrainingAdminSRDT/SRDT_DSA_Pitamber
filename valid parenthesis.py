83a55a994ed1cb9662bf995d80999b2b926b771c
Aditi Sahu <eraditisahu2006@gmail.com>
2026-06-11 13:53:46 +0530

    Add files via upload

---
+++ valid parenthesis.py
@@ -0,0 +1,21 @@
+def isValid(s: str) -> bool:
+    stack = []
+    
+    mapping = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    
+    for char in s:
+        # If closing bracket
+        if char in mapping:
+            top = stack.pop() if stack else '#'
+            
+            if mapping[char] != top:
+                return False
+        else:
+            # opening bracket
+            stack.append(char)
+    
+    return len(stack) == 0
