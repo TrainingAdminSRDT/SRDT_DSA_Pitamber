07a32d267c0d318dde0a6d957f12b0603cedfab2
mishrashradha229 <mishrashradha229@gmail.com>
2026-06-11 11:31:33 +0530

    updated palindrome solution

--- palindrone.java
+++ palindrone.java
@@ -5,16 +5,16 @@ public class palindrone {
     public static void main(String[] args) {
         int num=121;
         int temp=num;
-        rev=0;
+        int rev=0;
         while(num>0){
             int digit= num%10;
             rev=rev*10+digit;
             num=num/10;
         }
         if( temp==rev){
-            sout(true);
+            System.out.println("true");
             }else{
-                sout("false");
+                System.out.println("false");
             }
         
         
