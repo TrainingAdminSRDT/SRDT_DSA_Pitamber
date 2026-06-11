618952233feed22e7d0d3bb173eb84394085e091
mishrashradha229 <mishrashradha229@gmail.com>
2026-06-11 21:48:43 +0530

    doublyLL DSA Assignment

---
+++ DoublyLinkedList.java
@@ -0,0 +1,56 @@
+class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+public class DoublyLinkedList {
+    Node head;
+
+    // Insert at end
+    void insert(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+        newNode.prev = temp;
+    }
+
+    // Print list
+    void printList() {
+        Node temp = head;
+
+        System.out.print("Doubly Linked List: ");
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+
+        dll.insert(10);
+        dll.insert(20);
+        dll.insert(30);
+        dll.insert(40);
+
+        dll.printList();
+    }
+}
